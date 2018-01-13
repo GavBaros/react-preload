0495c1ae0a8ee3c38de90432894c14ceecb47681
Sebastian Siemssen <fubhy@fubhy.com>
2018-01-13 09:56:44 +0100

    Adding a simple demo.

--- packages/react-preload-demo/src/index.js
+++ packages/react-preload-demo/src/index.js
@@ -4,8 +4,43 @@ import './reset.scss';
 import './styles.scss';
 
 import ReactDOM from 'react-dom';
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
+import { Switch, Route } from 'react-router';
+import { BrowserRouter, Link } from 'react-router-dom';
+import { makeAsyncRouter } from 'react-router-preload';
+import { withPreloading } from 'react-preload-core';
 
-const App = () => <div>Hello World!</div>;
+const HomePage = () => 'Hello world!';
+const SyncPage = () => 'Sync page';
+const AsyncPageSlow = withPreloading(() => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), 1000);
+  });
+})(() => 'Async slow');
+
+const AsyncPageVerySlow = withPreloading(() => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), 3000);
+  });
+})(() => 'Async very slow');
+
+const AsyncRouter = makeAsyncRouter(BrowserRouter);
+
+const App = () => (
+  <AsyncRouter>
+    <Fragment>
+      <Link to="/">Home</Link>
+      <Link to="/sync">Sync</Link>
+      <Link to="/async-slow">Async Slow</Link>
+      <Link to="/async-very-slow">Async Very Slow</Link>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/sync" component={SyncPage} />
+        <Route path="/async-slow" component={AsyncPageSlow} />
+        <Route path="/async-very-slow" component={AsyncPageVerySlow} />
+      </Switch>
+    </Fragment>
+  </AsyncRouter>
+);
 
 ReactDOM.render(<App />, document.getElementById('app'));
