31ba80bc72f65cb947d6c38361429436f6080dd2
Sebastian Siemssen <fubhy@fubhy.com>
2018-01-14 09:37:53 +0100

    Remove peer dependencies.

--- packages/react-preload-apollo/src/index.js
+++ packages/react-preload-apollo/src/index.js
@@ -1,23 +1,17 @@
 // @flow
 
-import { graphql as graphqlOriginal } from 'react-apollo';
-import { withPreloading } from 'react-preload-core';
+import { withPreloading as doPreload } from 'react-preload-core';
 
-export const graphql = (query: any, options: any = {}) => {
-  const withPreloadedQuery = withPreloading((element, instance) => {
-    // Check if the current component is an Apollo query wrapper.
-    if (instance && typeof instance.fetchData === 'function') {
-      // Execute the graphql query for this component.
-      const fetch = instance.fetchData();
+export const withPreloading = doPreload((element, instance) => {
+  // Check if the current component is an Apollo query wrapper.
+  if (instance && typeof instance.fetchData === 'function') {
+    // Execute the graphql query for this component.
+    const fetch = instance.fetchData();
 
-      if (fetch && fetch.then && typeof fetch.then === 'function') {
-        return fetch;
-      }
+    if (fetch && fetch.then && typeof fetch.then === 'function') {
+      return fetch;
     }
+  }
 
-    return true;
-  });
-
-  return (component: React$Element<any>) =>
-    withPreloadedQuery(graphqlOriginal(query, options)(component));
-};
+  return true;
+});
--- packages/react-preload-universal-component/src/index.js
+++ packages/react-preload-universal-component/src/index.js
@@ -1,9 +1,8 @@
 // @flow
 
-import universalOriginal from 'react-universal-component';
-import { withPreloading } from 'react-preload-core';
+import { withPreloading as doPreload } from 'react-preload-core';
 
-const withPreloadedComponent = withPreloading(element => {
+export const withPreloading = doPreload(element => {
   // Check if the current component is a lazy component wrapper.
   if (element && element.type && typeof element.type.preload === 'function') {
     // Preload the component file.
@@ -16,6 +15,3 @@ const withPreloadedComponent = withPreloading(element => {
 
   return true;
 });
-
-export const universal = (...args: Array<any>) =>
-  withPreloadedComponent(universalOriginal(...args));
